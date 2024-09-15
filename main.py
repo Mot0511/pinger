cba7f975cf7c5a04f5e4c75aa547a825640211a7
Mot0511 <suvorov.matvej9@gmail.com>
2024-09-15 08:57:25 +0300

    _

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys
 import requests
 import time
 import random
+import datetime
 
 url = sys.argv[1]
 
 while True:
     requests.get(url)
     time.sleep(random.randint(1, 11))
+    print(f'[{datetime.datetime.now()}] - Работает')
